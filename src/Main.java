4414fc080ca21504cb27b91037d60b46b5ffbc6d
fforay <fforay@users.noreply.github.com>
2023-09-29 13:25:53 +0200

    Have a nice day

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Bonjour Monsieur Foray");
+
+        System.out.println("Merci, bonne journée à toi aussi.");
     }
 }
